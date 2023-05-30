6b46105d8d72ab454925714530a5a6a954ec6f1d
makenaaaaa <85456338+makenaaaaa@users.noreply.github.com>
2023-05-30 15:17:03 +0800

    Add files via upload

---
+++ azure_vars.tf
@@ -0,0 +1,13 @@
+// Set username of VM and DB
+variable "vm_user" {
+  type    = string
+}
+
+// Set password of VM and DB
+variable "vm_pass" {
+  type    = string
+}
+
+variable "resource_group" {
+  type = string
+}
